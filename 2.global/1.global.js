21061e1df41fce070082d7c0b85c4e5534035b66
UKkkkkkkkkk <825846102@qq.com>
2016-10-20 17:58:06 +0800

    time-timeEnd

--- 2.global/1.global.js
+++ 2.global/1.global.js
@@ -1,4 +1,17 @@
 /**
  * Created by Administrator on 2016/10/20.
  */
-console.log(global);
+//console.log(global);
+console.log("log");
+console.info("info");
+
+
+console.warn("warn");
+console.error("error");
+
+console.time("costTime");//costTime 之中代码运行的时间
+for(var i=0;i<100000000;i++){
+
+}
+
+console.timeEnd('costTime');
