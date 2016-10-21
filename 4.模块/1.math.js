be972e9b7cf16720d005daf2f0d0ac2948b974b6
UKkkkkkkkkk <825846102@qq.com>
2016-10-21 21:36:21 +0800

    exports 抛出接口，require 引入

---
+++ 4.模块/1.math.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/10/21.
+ */
+/**
+ * 私有，模块外无法访问
+ * @param a 
+ * @param b
+ * @returns {*}
+ */
+function add(a,b){
+    return a+b
+}
+exports.add=add;//抛出接口
+console.log(exports.add)
---
+++ 4.模块/2.usemath.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2016/10/21.
+ */
+var math=require("./1.math.js");
